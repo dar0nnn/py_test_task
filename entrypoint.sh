be66132ebd06fec842fa491a7f84b52cb41de918
Pavel Lukyanov <Pavel.Lukyanov@nexign.com>
2019-10-20 01:54:31 +0300

    fix migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,6 @@ echo "PostgreSQL started"
 
 # migrations
 python manage.py makemigrations meeting_registation
-#python manage.py migrate --fake-initial
-#python manage.py makemigrations
 python manage.py migrate
 # superuser
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'test@example.com', 'test')"
